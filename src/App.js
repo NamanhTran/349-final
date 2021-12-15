25d372e81700214e860a6dcc6d57f716d18a920b
Namanh Tran <NamanhDTran@gmail.com>
2021-12-14 20:39:05 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,140 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [curInput, setCurInput] = useState("");
+  const [prevOperator, setPrevOperator] = useState(null);
+  const [prevInput, setPrevInput] = useState(null);
+
+  const onPress = (e) => {
+    const key = e.target.innerHTML;
+    console.log(key);
+    const numSet = new Set(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']);
+
+    if (numSet.has(key)) {
+      setCurInput(curInput + key);
+    }
+
+    else if (key === "AC") {
+      setCurInput("");
+      setPrevOperator(null);
+      setPrevInput(null);
+    }
+
+    else if (key === "+/-") {
+      if (curInput === "") {
+        setCurInput("-");
+      }
+
+      else if (curInput[0] === '-') {
+        setCurInput(curInput.slice(1));
+      }
+
+      else {
+        setCurInput('-' + curInput);
+      }
+    }
+
+    else if (key === "*") {
+      if (prevInput !== null) {
+        setCurInput(parseFloat(prevInput) * parseFloat(curInput));
+      }
+
+      else {
+        setPrevInput(curInput);
+        setPrevOperator('*');
+        setCurInput("");
+      }
+    }
+
+    else if (key === "-") {
+      if (prevInput !== null) {
+        setCurInput(parseFloat(prevInput) - parseFloat(curInput));
+      }
+
+      else {
+        setPrevInput(curInput);
+        setPrevOperator('-');
+        setCurInput("");
+      }
+    }
+
+    else if (key === '+') {
+      if (prevInput !== null) {
+        setCurInput(parseFloat(prevInput) + parseFloat(curInput));
+      }
+
+      else {
+        setPrevInput(curInput);
+        setPrevOperator('+');
+        setCurInput("");
+      }
+    }
+
+    else if (key === '.') {
+      if (curInput.length === 0) {
+        setCurInput('.');
+      }
+
+      else if (curInput[curInput.length - 1] !== '.') {
+        setCurInput(curInput + '.');
+      }
+    }
+
+    else if (key === '=') {
+      if (prevInput !== null && curInput !== "") {
+        setCurInput((parseFloat(prevInput) * parseFloat(curInput)).toString());
+        switch (prevOperator) {
+          case '*': 
+          setCurInput((parseFloat(prevInput) * parseFloat(curInput)).toString());
+            break;
+          
+          case '-': 
+            setCurInput((parseFloat(prevInput) - parseFloat(curInput)).toString());
+            break;
+
+          case '+': 
+            setCurInput((parseFloat(prevInput) + parseFloat(curInput)).toString());
+            break;
+
+          case '/': 
+            setCurInput((parseFloat(prevInput) / parseFloat(curInput)).toString());
+            break;
+        }
+
+        setPrevInput(null);
+        setPrevOperator(null);
+      }
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label >Calculator:</label> 
+      <br/>
+      <input type="text" id="calculator-input" name="calculator-input" required value={curInput}></input>
+      <br />
+      <button type="button" onClick={onPress}>AC</button>
+      <button type="button" onClick={onPress}>+/-</button>
+      <button type="button" onClick={onPress}>/</button>
+      <br />
+      <button type="button" onClick={onPress}>7</button>
+      <button type="button" onClick={onPress}>8</button>
+      <button type="button" onClick={onPress}>9</button>
+      <button type="button" onClick={onPress}>*</button>
+      <br />
+      <button type="button" onClick={onPress}>4</button>
+      <button type="button" onClick={onPress}>5</button>
+      <button type="button" onClick={onPress}>6</button>
+      <button type="button" onClick={onPress}>-</button>
+      <br />
+      <button type="button" onClick={onPress}>1</button>
+      <button type="button" onClick={onPress}>2</button>
+      <button type="button" onClick={onPress}>3</button>
+      <button type="button" onClick={onPress}>+</button>
+      <br />
+      <button type="button" onClick={onPress}>0</button>
+      <button type="button" onClick={onPress}>.</button>
+      <button type="button" onClick={onPress}>=</button>
     </div>
   );
 }
